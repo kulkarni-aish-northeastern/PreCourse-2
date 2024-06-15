e14e58007d26744f5415a8365442fbaf270c6d9c
Aish <aishwarya@nachonacho.com>
2024-06-14 18:22:30 -0700

    Completed PreCourse2

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,3 +1,5 @@
+# Time Complexity : O(logn)
+# Space Complexity : O(1)
 # Python code to implement iterative Binary  
 # Search. 
   
@@ -6,7 +8,18 @@
 def binarySearch(arr, l, r, x): 
   
   #write your code here
-  
+  l = 0
+  r = len(arr) - 1
+  while l <= r:
+    mid = (l + r) // 2
+    if arr[mid] == x:
+      return mid
+    elif arr[mid] < x:
+      l = mid + 1
+    else:
+      r = mid - 1
+  return -1
+
     
   
 # Test array 
@@ -17,6 +30,6 @@ x = 10
 result = binarySearch(arr, 0, len(arr)-1, x) 
   
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index", result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
--- Exercise_3.py
+++ Exercise_3.py
@@ -1,20 +1,33 @@
+# Time Complexity : O(N)
+# Space Complexity : O(1)
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data 
+        self.next = None  
         
 class LinkedList: 
-  
+
     def __init__(self): 
+        self.head = None
         
   
     def push(self, new_data): 
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
         
   
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        print(slow.data)
 
 # Driver code 
 list1 = LinkedList() 
